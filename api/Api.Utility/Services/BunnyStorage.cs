8779e3951a20154174dfc83266c2192635b3ffbf
Saif <100407726+Saif435@users.noreply.github.com>
2023-02-28 09:46:47 +0200

    Add files via upload

---
+++ api/Api.Utility/Services/BunnyStorage.cs
@@ -0,0 +1,43 @@
+﻿using API.Utility.Services.Interfaces;
+using BunnyCDN.Net.Storage;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Utility.Services
+{
+    public class BunnyStorage : IBunnyStorage
+    {
+        private readonly IConfiguration _config;
+        private readonly string _zoneName;
+        private readonly string _accessKey;
+        private readonly string _zoneRegion;
+
+        public BunnyStorage(IConfiguration config)
+        {
+            _config = config;
+            _zoneName = _config["BunnyStorage:ZoneName"];
+            _accessKey = _config["BunnyStorage:AccessKey"];
+            _zoneRegion = _config["BunnyStorage:ZoneRegion"];
+        }
+
+        public async Task<bool> Delete(string fileName)
+        {
+            var bunnyCDNStorage = new BunnyCDNStorage(_zoneName, _accessKey, _zoneRegion);
+
+            if (fileName != null)
+            {
+                await bunnyCDNStorage.DeleteObjectAsync("/" + _zoneName + "/" + fileName);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task Upload(string fileName, Stream fileContent)
+        {
+            var bunnyCDNStorage = new BunnyCDNStorage(_zoneName, _accessKey, _zoneRegion);
+            await bunnyCDNStorage.UploadAsync(fileContent, "/" + _zoneName + "/" + fileName);
+        }
+    }
+}
---
+++ api/Api.Utility/Services/Interfaces/IBunnyStorage.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Utility.Services.Interfaces
+{
+    public interface IBunnyStorage
+    {
+        Task Upload(string fileName, Stream fileContent);
+        Task<bool> Delete(string fileName);
+    }
+}
